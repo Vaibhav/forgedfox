2c7b49a2a5af0b9d2e13b744099a4f6ebf8be075
Vaibhav <ishuvaibhav@gmail.com>
2018-03-25 10:31:02 -0400

    finish

--- src/VerifyResults.js
+++ src/VerifyResults.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import { verifyHash, getUser } from './IPFS';
 import RefreshIndicator from 'material-ui/RefreshIndicator';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 class VerifyResults extends Component {
   constructor() {
     super();
-    this.state = { isLoading: true };
+    this.state = { isLoading: true, address: false};
   }
 
+  handleChange = (event) => {
+    this.setState({
+      contract_address: event.target.value,
+    });
+  };
+
+
   componentDidMount() {
-    const USER_ADDRESS = '0x8FDfccE1d7Ff2F00D86c68B7a0e50A074FB76b26' ;
+    const CONTRACT_ADDRESS = this.state.contract_address;
 
     const verFile = () => {
-      verifyHash(USER_ADDRESS, this.props.result)
+      verifyHash(CONTRACT_ADDRESS, this.props.result)
       .then(verify => {
         this.setState({ verify, isLoading: false })
         console.log('FINISHED block await call!', verify);
@@ -22,13 +31,56 @@ class VerifyResults extends Component {
 
     console.log("initial state of VF");
     console.log(this.state);
-    verFile();
+    if (this.state.address) verFile();
   }
 
   render() {
-    if (!this.state.isLoading) {
+
+    const verFile = () => {
+      verifyHash(this.state.contract_address, this.props.result)
+      .then(verify => {
+        this.setState({ verify, isLoading: false })
+        console.log('FINISHED block await call!', verify);
+      })
+      .catch(err => console.log(err));
+    }
+
+    function onclickHandler() {
+      this.setState({
+        address: true,
+      });
+      verFile();
+    }
+
+
+    if (!this.state.address) {
+      return (
+        <div>
+        <TextField
+            hintText="Enter Conctract Address"
+            floatingLabelText="Conctract Address"
+            multiLine={true}
+            rows={2}
+            floatingLabelStyle={{ position: 'relative'}}
+            onChange={this.handleChange}
+          />
+      <RaisedButton label="Add File" primary={true} style={{ margin: 12 }} onClick={onclickHandler.bind(this)}/>
+      </div>
+      );
+
+    } else if (!this.state.isLoading) {
       return (
         <div className='results'>
+        <h2> 
+          {this.state.verify.found ? "File Could Not Be Found" : 
+          <div> 
+            ipfsPath : {this.state.verify.ipfsPath}
+            addedAt : {this.state.verify.addedAt}
+            creator : {this.state.verify.creator}
+            fileHash : {this.state.verify.fileHash}
+            metaDataHash : {this.state.verify.metadataHash}
+          </div>}
+        </h2>
         </div>
       );
     }
